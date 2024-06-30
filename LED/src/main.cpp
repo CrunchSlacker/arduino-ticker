a10fbce2d359681d049526a93fa7793dd93686ba
CrunchSlacker <cyrusyong1@gmail.com>
2024-06-30 19:32:55 -0400

    now turns servo to correct angle based off of price

--- LED/src/main.cpp
+++ LED/src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <Servo.h>
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+Servo servoOne;
 
 void setup()
 {
@@ -8,6 +11,7 @@ void setup()
   Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.clear();
+  servoOne.attach(13);
 }
 
 void loop()
@@ -15,9 +19,18 @@ void loop()
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0)
   {
-    String data = Serial.readString();
+    String data = Serial.readStringUntil('$');
+    data.trim();
 
+    String num = Serial.readString();
+    num.trim();
+
+    int hundreds = String(num[0]).toInt();
+    int hundredsAngle = map(hundreds, 0, 9, 0, 179);
+    Serial.print(hundredsAngle);
+    servoOne.write(hundredsAngle);
+    
     lcd.clear();
-    lcd.print(data);
+    lcd.print(num);
   }
 }
