6b9b79daa53e03e311abec8939dcec860c936a26
CrunchSlacker <cyrusyong1@gmail.com>
2024-06-30 19:33:33 -0400

    formats price correctly for hundreds

--- prices/moola.js
+++ prices/moola.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import "dotenv/config";
 import { restClient } from "@polygon.io/client-js";
 import { SerialPort } from "serialport";
 const rest = restClient(process.env.POLYGON_API);
@@ -16,22 +16,52 @@ const port = new SerialPort({
 
 async function getInfo() {
   const data = await rest.stocks.aggregates(
-    "NVDA",
+    "COST",
     "1",
-    "day",
-    "2024-06-26",
-    "2024-06-26"
+    "minute",
+    "2024-06-27",
+    "2024-06-28",
+    { sort: "desc", limit: "5" }
   );
-
+  console.log(data);
   return { ticker: data.ticker, closePrice: data.results[0].c };
 }
 
 async function main() {
   const stockInfo = await getInfo();
 
+  const formatedPrice = await formatPrice(stockInfo.closePrice);
+
   setTimeout(() => {
-    port.write(stockInfo.ticker + ": $" + stockInfo.closePrice.toString());
+    port.write(
+      stockInfo.ticker + ": $" + formatedPrice
+    );
   }, 3000);
 }
 
+async function formatPrice(price) {
+  let stockPrice = parseFloat(price).toFixed(2);
+
+  let arrayPrice = [];
+
+  //Pushes all numbers before "." to array
+  for (let i = 0; i < stockPrice.length; i++) {
+    if (stockPrice[i] !== ".") {
+      arrayPrice.push(stockPrice[i]);
+    } else {
+      break;
+    }
+  }
+
+  //Adds zeros to fill in space, example: format 00#.##
+  let howManyZeros = 3 - arrayPrice.length;
+  for (let i = 0; i < howManyZeros; i++) {
+    arrayPrice.unshift(0);
+  }
+
+  //Returns formatted price
+  return arrayPrice.join('') + stockPrice.slice(stockPrice.indexOf('.'));
+}
+
 main();
+
